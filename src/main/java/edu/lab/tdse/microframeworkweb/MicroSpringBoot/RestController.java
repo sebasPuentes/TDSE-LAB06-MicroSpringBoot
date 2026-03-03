796f62051f3ccca0ac9826aad1b25e63b96aa814
unknown <juan.puentes@labinfo.is.escuelaing.edu.co>
2026-03-03 11:15:01 -0500

    fix: Resstcontroler

--- src/main/java/edu/lab/tdse/microframeworkweb/MicroSpringBoot/RestController.java
+++ src/main/java/edu/lab/tdse/microframeworkweb/MicroSpringBoot/RestController.java
@@ -5,7 +5,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
-@Target(ElementType.METHOD)
+@Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface RestController {
 
